4510110c41f6bc7b25c49caba4be051621a3eed4
Ryan Goggin <ryangoggin1@gmail.com>
2023-04-17 23:08:07 -0400

    got faster solution for 26

--- 26-remove-dupe-from-sorted-arr.js
+++ 26-remove-dupe-from-sorted-arr.js
@@ -5,24 +5,45 @@ return # of unique elements
 */
 
 /*
-1.) make a set from nums already in nums
+FASTER
+1.) make a set from nums
+2.) convert the set to an array to iterate over it
+3.) reassign corresponding indices in nums to the index in the set
+4.) return size of the set
+*/
+
+// faster O(n), n is unique nums in nums
+var removeDuplicates = function(nums) {
+    let unique = new Set(nums);
+    let uniqueArr = Array.from(unique);
+
+    for (let i = 0; i < uniqueArr.length; i++) {
+        nums[i] = uniqueArr[i]
+    }
+
+    return unique.size;
+};
+
+/*
+SLOWER
+1.) make an empty set
 2.) iterate over nums checking if current num in the set
 3.) if it is, splice it out of nums
 4.) if it is not, add it to the set and continue
 */
 
-// slower O(n+m)
-var removeDuplicates = function(nums) {
-    let unique = new Set();
+// // slower O(n+m), n is nums in nums, m is num of dupes in nums
+// var removeDuplicates = function(nums) {
+//     let unique = new Set();
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] in unique) {
-            nums.splice(i, 1);
-            i--;
-        } else {
-            unique.add(nums[i]);
-        }
-    }
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] in unique) {
+//             nums.splice(i, 1);
+//             i--;
+//         } else {
+//             unique.add(nums[i]);
+//         }
+//     }
 
-    return nums.length;
-};
+//     return nums.length;
+// };
