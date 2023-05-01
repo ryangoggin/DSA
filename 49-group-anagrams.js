64e5390dc168763f3ab5cbf9e515c3e2a7e49560
Ryan Goggin <ryangoggin1@gmail.com>
2023-05-01 19:12:33 -0400

    did 49 in js

--- 49-group-anagrams.js
+++ 49-group-anagrams.js
@@ -3,4 +3,20 @@ given array of strings, group the anagrams together, can return the answer in an
 */
 
 /*
+look at py for general thought process
 */
+
+var groupAnagrams = function(strs) {
+    let anagrams = {};
+
+    for (word of strs) {
+        let anagram = word.split("").sort().join("");
+        if (!anagrams[anagram]) {
+            anagrams[anagram] = [word];
+        } else {
+            anagrams[anagram].push(word);
+        }
+    }
+
+    return Object.values(anagrams);
+};
