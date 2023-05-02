6d000e0ab2c064769b4e3edd1344f33f612cedde
Ryan Goggin <ryangoggin1@gmail.com>
2023-05-02 17:38:09 -0400

    did 100 in py

---
+++ 100-same-tree.py
@@ -0,0 +1,10 @@
+# look at js file for reasoning
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if (not p and q) or (p and not q) or (p.val != q.val):
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
