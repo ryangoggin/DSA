49755195cec6543155b4100d20225794e403aed6
Ryan Goggin <ryangoggin1@gmail.com>
2023-05-03 19:51:47 -0400

    did 206 in python

--- 206-reverse-linked-list.py
+++ 206-reverse-linked-list.py
@@ -1,1 +1,13 @@
 # look at js file for reasoning/explanation
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous = None
+
+        while head:
+            old_next = head.next
+            head.next = previous
+            previous = head
+            head = old_next
+
+        return previous
