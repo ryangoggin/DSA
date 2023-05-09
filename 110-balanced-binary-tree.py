deca8c18fac3c4a44f7846aab6ca8915ab587c59
Ryan Goggin <ryangoggin1@gmail.com>
2023-05-09 16:27:35 -0400

    finished 110 in py

--- 110-balanced-binary-tree.py
+++ 110-balanced-binary-tree.py
@@ -6,3 +6,19 @@
 # 2.) left and right heights are recursive calls on the respective children nodes of root
 # 3.) if left or right height are negative or the diff between subtree heights are greater than 1, return -1
 # 4.) return max of left and right height incrememnted by 1
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return (self.Height(root) >= 0)
+
+    def Height(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return 0
+
+        left_height = self.Height(root.left)
+        right_height = self.Height(root.right)
+
+        if left_height < 0 or right_height < 0 or abs(left_height - right_height) > 1:
+            return -1
+        return max(left_height, right_height) + 1
