395cac5aa5a2c4c8d8196b7c15ab7d0c7a1550cf
Ryan Goggin <ryangoggin1@gmail.com>
2023-05-21 18:44:21 -0400

    did 203 in py

--- 203-remove-linked-list-elements.py
+++ 203-remove-linked-list-elements.py
@@ -1,1 +1,22 @@
 # see js file for thought process
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        while head:
+            if head.val == val:
+                head = head.next
+            else:
+                break
+
+        curr = head
+
+        while curr and curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return head
