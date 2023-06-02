a9ab140a7fce60b297702aff1a5578351a9f8918
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-01 22:36:03 -0400

    added faster py solution for 437

--- 437-path-sum-iii.py
+++ 437-path-sum-iii.py
@@ -29,3 +29,40 @@ class Solution:
 
         dfs(root, targetSum)
         return res
+
+
+# not my solution, but this is faster by using a hash map:
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def pathSum(self, root, sum):
+
+        def dfs(sumHash, prefixSum, node):
+
+            if not node:
+                return 0
+
+			# Sum of current path
+            prefixSum += node.val
+
+			# number of paths that ends at current node
+            path = sumHash[prefixSum - sum]
+
+			# add currentSum to prefixSum Hash
+            sumHash[prefixSum] += 1
+
+			# traverse left and right of tree
+            path += dfs(sumHash, prefixSum, node.left) + dfs(sumHash, prefixSum, node.right)
+
+		    # remove currentSum from prefixSum Hash
+            sumHash[prefixSum] -= 1
+
+            return path
+
+        # depth first search, initialize sumHash with prefix sum of 0, occurring once
+        return dfs(collections.defaultdict(int, {0: 1}), 0, root)
