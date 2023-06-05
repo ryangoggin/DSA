a27b70936504c420949af02279afbf2783e52177
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-05 15:45:39 -0400

    219 in py

---
+++ 219-contains-dupe-ii.py
@@ -0,0 +1,13 @@
+# look at js file for thought process
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+
+        for i in range(len(nums)):
+            if nums[i] in hashmap and i - hashmap[nums[i]] <= k:
+                return True
+
+            hashmap[nums[i]] = i
+
+        return False
