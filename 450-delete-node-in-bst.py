a8172b0821fbef61d7c371dd2cc3613c1e70b1bc
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-08 19:06:50 -0400

    finished 450 in py

---
+++ 450-delete-node-in-bst.py
@@ -0,0 +1,29 @@
+# see js file for thought process
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        def dfsDel(node):
+            if not node:
+                return node
+
+            if node.val == key:
+                if not node.left:
+                    return node.right
+                if not node.right:
+                    return node.left
+
+                curr = node.right
+                while curr.left:
+                    curr = curr.left
+                curr.left = node.left
+
+                return node.right
+
+            if node.val > key:
+                node.left = dfsDel(node.left)
+            else:
+                node.right = dfsDel(node.right)
+
+            return node
+
+        return dfsDel(root)
