bd8e151cf8bec22bad525347297235b510160379
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-09 18:29:12 -0400

    744 in py

---
+++ 744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,9 @@
+# see js file for thought process
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for letter in letters:
+            if letter > target:
+                return letter
+
+        return letters[0]
