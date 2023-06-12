93d3cc39cd6517f55ce49e229632fa3eb672817d
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-12 17:37:56 -0400

    faster 933 in js

--- 933-number-recent-calls.js
+++ 933-number-recent-calls.js
@@ -25,12 +25,6 @@ recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], retu
 recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
 */
 
-/*
-really slow, really space inefficient
-1.) make a copy of the counter
-2.) while the last element of the counter is greater than or equal to t-3000 pop it off the copy and push into a res array
-3.) return the length of the res array
-*/
 var RecentCounter = function() {
     this.counter = [];
 };
@@ -42,16 +36,46 @@ var RecentCounter = function() {
 RecentCounter.prototype.ping = function(t) {
     this.counter.push([t]);
 
-    let res = [];
-    let counterCopy = [...this.counter];
-
-    while (counterCopy[counterCopy.length - 1][0] >=  t - 3000) {
-        res.push(counterCopy.pop());
-        if (counterCopy.length === 0) break;
+    while (this.counter[0] < t - 3000) {
+        this.counter.shift();
     }
 
-    return res.length;
+    return this.counter.length;
 };
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
+
+/*
+really slow, really space inefficient
+1.) make a copy of the counter
+2.) while the last element of the counter is greater than or equal to t-3000 pop it off the copy and push into a res array
+3.) return the length of the res array
+*/
+// var RecentCounter = function() {
+//     this.counter = [];
+// };
+
+// /**
+//  * @param {number} t
+//  * @return {number}
+//  */
+// RecentCounter.prototype.ping = function(t) {
+//     this.counter.push([t]);
+
+//     let res = [];
+//     let counterCopy = [...this.counter];
+
+//     while (counterCopy[counterCopy.length - 1][0] >=  t - 3000) {
+//         res.push(counterCopy.pop());
+//         if (counterCopy.length === 0) break;
+//     }
+
+//     return res.length;
+// };
 
 /**
  * Your RecentCounter object will be instantiated and called as such:
