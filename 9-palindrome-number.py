bb32ea7b4724ab2139bb41a73d399ddd9e6973d0
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-13 00:43:00 -0400

    9 in py

---
+++ 9-palindrome-number.py
@@ -0,0 +1,17 @@
+# see js file for thougt process
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string = str(x)
+        lst = list(string)
+        start = 0
+        end = len(lst) - 1
+
+        while start <= end:
+            if lst[start] != lst[end]:
+                return False
+
+            start += 1
+            end -= 1
+
+        return True
