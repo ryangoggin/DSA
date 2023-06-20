4ea7e875f7e6d43a3f09aed0fe2b28dec1d137ad
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-20 12:06:44 -0400

    169 in py

--- 169-majority-element.py
+++ 169-majority-element.py
@@ -1,1 +1,17 @@
 # see js file for thought process
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq = {}
+
+        for num in nums:
+            if num not in freq:
+                freq[num] = 1
+            else:
+                freq[num] += 1
+
+        items = list(freq.items())
+
+        for item in items:
+            if item[1] > len(nums)/2:
+                return item[0]
