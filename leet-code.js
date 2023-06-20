a9a98a438c9c178a1f8a3b076cb8612891a6b5af
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-20 16:45:52 -0400

    added nested remove 0s solution from aileen's SF interview

--- leet-code.js
+++ leet-code.js
@@ -1,4 +1,32 @@
+function filterArray(arr) {
+    // track the res
+    let res = [];
 
+    // use a for loop to iterate through the input array
+    for (let i = 0; i < arr.length; i++) {
+      // if conditionals: if the ele != 0 push to res
+      let ele = arr[i];
+
+      // if cond: if type of ele is an array use innerRes = [];
+      if (Array.isArray(ele)) {
+        // then we will call our function recursively to flatten our array
+        res.push(filterArray(ele));
+      } else if (ele === 0) {
+        continue
+      } else {
+        res.push(ele);
+      }
+    }
+
+    // return res
+    return res;
+
+  };
+
+  let input = [0, 0, 5, [0, 0], [0, [[[0,16],16],16]]]
+
+  let res = filterArray(input);
+  console.log(res[2][0]);
 /*
 Weird twoSum that is almost O(n):;
 JUST US A HASH MAP THO
