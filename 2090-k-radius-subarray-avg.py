998d14e49f8319e9922b8cf356a648705be6dbaa
Ryan Goggin <ryangoggin1@gmail.com>
2023-06-20 18:31:15 -0400

    2090 in py

--- 2090-k-radius-subarray-avg.py
+++ 2090-k-radius-subarray-avg.py
@@ -1,1 +1,16 @@
 # see js file for thought process
+
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        res = [-1] * len(nums)
+        sum = 0
+
+        for i in range(len(nums)):
+            sum += nums[i]
+
+            if i >= 2*k:
+                avg = sum // (2*k + 1)
+                res[i - k] = avg
+                sum -= nums[i - 2*k]
+
+        return res
