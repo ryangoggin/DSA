8e434c1a75e34191962a5483380181c1d77a43e3
Ryan Goggin <ryangoggin1@gmail.com>
2023-07-02 10:53:39 -0400

    189 in js... too slow

---
+++ 189-rotate-array.js
@@ -0,0 +1,35 @@
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+/*
+too slow, last test case times out...
+1.) iterate k times
+2.) pop off last item, unshift to front of the array
+*/
+
+var rotate = function(nums, k) {
+    for (let i = 0; i < k; i++) {
+        let num = nums.pop();
+        nums.unshift(num);
+    }
+
+    return nums
+};
