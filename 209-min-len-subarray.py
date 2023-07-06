8966d702a81bcdba61f101c1d4e88d21dd9d91a4
Ryan Goggin <ryangoggin1@gmail.com>
2023-07-05 21:01:05 -0400

    209 in py

--- 209-min-len-subarray.py
+++ 209-min-len-subarray.py
@@ -1,1 +1,24 @@
 # see js file for thought process
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        first = 0
+        last = 0
+        curr_sum = 0
+        min_len = 0
+
+        while last < len(nums):
+            curr_sum += nums[last]
+
+            while curr_sum >= target:
+                curr_len = last - first + 1
+                if min_len == 0:
+                    min_len = curr_len
+                else:
+                    min_len = min(curr_len, min_len)
+                curr_sum -= nums[first]
+                first += 1
+
+            last += 1
+
+        return min_len
