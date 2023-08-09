1a201c5d93e7672add2c3a81866c8d838f900082
Ryan Goggin <ryangoggin1@gmail.com>
2023-08-08 20:24:19 -0400

    33 in py

---
+++ 33-search-rotated-sorted-array.py
@@ -0,0 +1,25 @@
+# see js file for thought process
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums) - 1
+
+        while lo <= hi:
+            mid = floor((lo+hi)/2)
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target and target <= nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                if nums[mid] <= target and target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+
+        return -1
