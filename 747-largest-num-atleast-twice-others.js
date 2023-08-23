fca4a2738582b74c8a601c0b268465bfcd772659
Ryan Goggin <ryangoggin1@gmail.com>
2023-08-23 13:52:08 -0400

    747 faster js solution

--- 747-largest-num-atleast-twice-others.js
+++ 747-largest-num-atleast-twice-others.js
@@ -19,6 +19,36 @@ Explanation: 4 is less than twice the value of 3, so we return -1.
 */
 
 /*
+faster O(n) one pass solution
+1.) have a first and second variable both start at negative infinity
+2.) have a res variable start at 0
+3.) iterate over nums
+4.) if the current num is greater than first, set second to the former first, set first to current num, and set res to i
+5.) otherwise if current num is greater than second but not first, set second to that num
+6.) after iterating, return a ternary where the conditional is first being greater than or equal to 2 times second
+7.) if it is, return res, otherwise return -1
+*/
+
+var dominantIndex = function(nums) {
+    let first = -Infinity;
+    let second= -Infinity;
+    let res = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > first) {
+            second = first;
+            first = nums[i];
+            res = i;
+        } else if (nums[i] > second) {
+            second = nums[i];
+        }
+    }
+
+    return first >= 2*second ? res : -1;
+};
+
+/*
+slower O(n*log(n)) becuase it uses sort
 1.) make a deep copy of nums and sort in descending order
 2.) check if first index is double or greater of second index
 3.) if it is return the index of the first index in nums
