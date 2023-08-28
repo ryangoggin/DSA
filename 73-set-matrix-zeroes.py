1266a59798def80d34816e2c6499a0d8d130f48b
Ryan Goggin <ryangoggin1@gmail.com>
2023-08-28 18:50:18 -0400

    73 in py with thought process written out

--- 73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes.py
@@ -1,4 +1,25 @@
+# Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+# You must do it in place.
 
+# Example 1:
+
+# Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+# Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+# Example 2:
+
+
+# Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+# Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+####################################################################
+# iterative approach
+# time: O(mxn) where m and n are dimensions of the matrix
+# 1.) make two sets to hold the cols and rows that have 0s and need to be converted
+# 2.) iterate over the matrix and add row and col to the appropriate sets whenever there's a 0
+# 3.) iterate over the rows in the row set and iterate over each col, convert all to 0
+# 4.) iterate over the cols in the col set and iterate over each row, convert all to 0
+# 5.) return matrix
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
