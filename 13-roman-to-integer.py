520c7df3f78cfa62414868bc28ad6cbefbf62f1d
Ryan Goggin <ryangoggin1@gmail.com>
2023-09-07 21:23:05 -0400

    13 in py

---
+++ 13-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        num_map = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        }
+
+        res = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1:
+                if num_map[s[i]] < num_map[s[i+1]]:
+                    res -= num_map[s[i]]
+                else:
+                    res += num_map[s[i]]
+            else:
+                res += num_map[s[i]]
+
+        return res
