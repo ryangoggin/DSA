e19c9de99ba559007b6566326c8d61a12bcc9501
Ryan Goggin <ryangoggin1@gmail.com>
2023-09-27 02:25:26 -0400

    more legit 880 js solution

--- 880-decoded-string-at-index.js
+++ 880-decoded-string-at-index.js
@@ -1,3 +1,46 @@
+/*
+You are given an encoded string s. To decode the string to a tape, the encoded string is read one character at a time and the following steps are taken:
+
+If the character read is a letter, that letter is written onto the tape.
+If the character read is a digit d, the entire current tape is repeatedly written d - 1 more times in total.
+Given an integer k, return the kth letter (1-indexed) in the decoded string.
+
+Example 1:
+
+Input: s = "leet2code3", k = 10
+Output: "o"
+Explanation: The decoded string is "leetleetcodeleetleetcodeleetleetcode".
+The 10th letter in the string is "o".
+
+Example 2:
+
+Input: s = "ha22", k = 5
+Output: "h"
+Explanation: The decoded string is "hahahaha".
+The 5th letter is "h".
+
+Example 3:
+
+Input: s = "a2345678999999999999999", k = 1
+Output: "a"
+Explanation: The decoded string is "a" repeated 8301530446056247680 times.
+The 1st letter is "a".
+*/
+
+
+/*
+more legit solution
+whole lot of big brain going on here... ~~ is double bitwise operator... works as faster Math.floor but only for positive numbers
+*/
+
+var decodeAtIndex = function(S, K) {
+    for (let i = 0, j = 0, n; true; i++)
+        if (S[i] < 10) {
+            n = ~~S[i] * j
+            if (K > n) j = n
+            else return decodeAtIndex(S, K % j || j)
+        } else if (++j === K) return S[i]
+};
 
 
 /*
