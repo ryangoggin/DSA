32ed260c9f3a285006fe1a3f804ea19a58102692
Ryan Goggin <ryangoggin1@gmail.com>
2023-10-04 01:16:54 -0400

    706 in py

---
+++ 706-design-hash-map.py
@@ -0,0 +1,22 @@
+# see js file for thought process
+
+class MyHashMap:
+
+    def __init__(self):
+        self.map = {}
+
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+
+    def get(self, key: int) -> int:
+        if key in self.map:
+            return self.map[key]
+        else:
+            return -1
+
+
+    def remove(self, key: int) -> None:
+        if key in self.map:
+            del self.map[key]
