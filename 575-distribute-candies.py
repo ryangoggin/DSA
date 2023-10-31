226364cd7d146faee28b9f3f808a8f1169cf8ee8
Ryan Goggin <ryangoggin1@gmail.com>
2023-10-31 18:13:10 -0400

    575 in py

---
+++ 575-distribute-candies.py
@@ -0,0 +1,18 @@
+# see js file for thought process
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        n = len(candyType)
+        maxCandy = int(n/2)
+        res = 0
+        unique = set()
+
+        for i in range(n):
+            if maxCandy == res:
+                return maxCandy
+
+            if candyType[i] not in unique:
+                unique.add(candyType[i])
+                res += 1
+            else:
+                continue
