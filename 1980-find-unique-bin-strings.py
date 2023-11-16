4cbc30cafbd7595c5601c1eea84c2eea2a6e7f37
Ryan Goggin <ryangoggin1@gmail.com>
2023-11-16 17:48:20 -0500

    1980 in py

---
+++ 1980-find-unique-bin-strings.py
@@ -0,0 +1,13 @@
+# see js file for thought process
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        res = ""
+
+        for i in range(len(nums)):
+            if nums[i][i] == "0":
+                res += "1"
+            else:
+                res += "0"
+
+        return res
