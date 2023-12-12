8afc22a84b33d5073a27801f2aa6021b268933dc
Ryan Goggin <ryangoggin1@gmail.com>
2023-12-12 00:48:19 -0500

    1464 in py

--- 1464-max-product-two-array-elements.py
+++ 1464-max-product-two-array-elements.py
@@ -5,3 +5,19 @@ class Solution:
         nums.sort(reverse=True)
 
         return (nums[0]-1) * (nums[1]-1)
+
+# faster O(n) solution
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        largest = float('-inf')
+        second_largest = nums[0]
+
+        for num in nums:
+            if num > largest:
+                second_largest = largest
+                largest = num
+            elif largest >= num > second_largest:
+                second_largest = num
+
+        return (largest - 1) * (second_largest - 1)
